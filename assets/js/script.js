8642c87d60a6dfa9a728160eaa714953edc32015
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-30 16:10:28 +0800

    Add core function for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+
+
+function runGame()
+
+function checkAnswer()
+
+function calculateCorrectAnswers()
+
+function incrementScore()
+
+function incrementWrongAnswer()
+
+function displayAdditionQuestion()
+
+function displaySubtractQuestion()
+
+function displayMultiplyQuestion()
+
+function displayDivideQuestion()
