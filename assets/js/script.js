6a0d24e327264b907a37f874ece41d71e4be8fe6
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-30 18:03:12 +0800

    Add division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,8 @@ function runGame(gameType) {
           displaySubtractQuestion(num1, num2);
      } else if (gameType === 'multiply') {
           displayMultiplyQuestion(num1, num2);
+     } else if (gameType === 'division') {
+          displayMultiplyDivision(num1, num2);
      } else {
           alert(`Unknown game type: ${gameType}`);
           throw `Unknown game type: ${gameType}. Aborting!`;
@@ -76,6 +78,8 @@ function calculateCorrectAnswer() {
           return [operand1 - operand2, 'subtract'];
      } else if (operator === '*') {
           return [operand1 * operand2, 'multiply'];
+     } else if (operator === '/') {
+          return [operand1 / operand2, 'division'];
      } else {
           alert(`Unimplemented operator ${operator}`);
           throw `Unimplemented operator ${operator}. Aborting!`;
@@ -116,4 +120,10 @@ function displayMultiplyQuestion(operand1, operand2) {
      document.getElementById('operand1').textContent = operand1;
      document.getElementById('operand2').textContent = operand2;
      document.getElementById('operator').textContent = '*';
+}
+
+function displayMultiplyDivision(operand1, operand2) {
+     document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operator').textContent = '/';
 }
